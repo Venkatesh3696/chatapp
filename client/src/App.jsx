585ef4a0dfd4b05f13f99a48fe49c026efba724e
Venkatesh3696 <venkateshpentakota888@gmail.com>
2025-05-14 15:07:39 +0530

    fixed navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Join from "./pages/Join";
 
 function App() {
-  const userName = localStorage.getItem("chat_username");
+  const userName = sessionStorage.getItem("chat_username");
   return (
     <BrowserRouter>
       <div className="h-screen w-screen flex overflow-auto bg-gray-700">
